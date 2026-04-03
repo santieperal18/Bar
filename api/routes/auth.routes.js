5a8225af86022862c6b9f83e9b74a66beb10114f
santino <santinoperal123@gmail.com>
2026-04-03 18:33:38 -0300

    hola3

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -31,8 +31,8 @@ router.post("/login", [
     // Enviar refresh token como HttpOnly cookie (mas seguro)
     res.cookie("refreshToken", resultado.refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 días
     });
 
